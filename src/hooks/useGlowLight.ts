4c1d0c0c0ba0c0036234e30acef694636df9bfdf
mydearxym <mydearxym@qq.com>
2024-10-30 15:35:23 +0800

    fix: colors adjust

--- src/hooks/useGlowLight.ts
+++ src/hooks/useGlowLight.ts
@@ -1,14 +1,31 @@
 import type { TGlowEffect } from '~/spec'
 
+import METRIC from '~/const/metric'
+import { GLOW_EFFECT_NAME, GLOW_OPACITY } from '~/const/glow_effect'
+
 import useSubStore from '~/hooks/useSubStore'
+import useMetric from '~/hooks/useMetric'
+import useTheme from '~/hooks/useTheme'
 
 export default (): TGlowEffect => {
   const dashboard = useSubStore('dashboard')
   const { wallpaper } = useSubStore('wallpaper')
 
+  const { isLightTheme } = useTheme()
+  const metric = useMetric()
+
   const { glowType, glowFixed, glowOpacity } = dashboard
 
   const changeGlowEffect = (glowType: string): void => dashboard.commit({ glowType })
+
+  if (metric === METRIC.HOME && !glowType) {
+    return {
+      glowType: GLOW_EFFECT_NAME.ORANGE_PURPLE,
+      glowFixed: false,
+      glowOpacity: isLightTheme ? GLOW_OPACITY.WEEK : GLOW_OPACITY.NORMAL,
+      changeGlowEffect,
+    }
+  }
 
   return {
     glowType: wallpaper && glowType,
