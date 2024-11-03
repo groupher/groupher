4b896b54ef8320da3fddd2e340ac57cbd1527274
mydearxym <mydearxym@qq.com>
2024-11-03 14:48:13 +0800

    chore: wip

--- src/app/apply/community/page.tsx
+++ src/app/apply/community/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = 'force-static'
+
 import CommunityEditor from '~/containers/editor/CommunityEditor'
 
 const ApplyCommunity = () => {
