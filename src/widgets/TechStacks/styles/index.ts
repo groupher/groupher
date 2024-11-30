51d81b9b8188840e6af263ccb020cabe03cc0aca
mydearxym <mydearxym@qq.com>
2024-11-30 15:06:29 +0800

    chore: clean up

--- src/widgets/TechStacks/styles/index.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from '~/css'
-
-export const Wrapper = styled.div`
-  ${css.row('align-start')};
-  margin-top: 8px;
-  margin-bottom: 8px;
-`
-
-export const Title = styled.div``
