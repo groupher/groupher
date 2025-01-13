589276972230594f3c6a8ce9e0a3ec5f24197a20
mydearxym <mydearxym@qq.com>
2025-01-13 16:03:52 +0800

    chore: wip

--- src/widgets/ArticleCard/index.tsx
+++ src/widgets/ArticleCard/index.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react'
+import Link from 'next/link'
 
 import type { TArticle } from '~/spec'
 import { THREAD } from '~/const/thread'
 import { cutRest } from '~/fmt'
 import { previewArticle } from '~/signal'
 import useViewingCommunity from '~/hooks/useViewingCommunity'
-import useIsArticleViewing from '~/hooks/useIsArticleViewing'
 
 import ArticleReadLabel from '~/widgets/ArticleReadLabel'
 import ArticlePinLabel from '~/widgets/ArticlePinLabel'
@@ -14,29 +14,31 @@ import ArticleImgWindow from '~/widgets/ArticleImgWindow'
 
 import Footer from './Footer'
 
-import { Wrapper, Title, PinHintDot, ViewedHintDot } from './styles'
+import useSalon from './salon'
 
 export type TProps = {
   data: TArticle
 }
 
 const ArticleCard: FC<TProps> = ({ data }) => {
+  const s = useSalon()
+
   const { slug } = useViewingCommunity()
   const { innerId, title, digest, isPinned, viewerHasViewed } = data
-  const isViewing = useIsArticleViewing(data)
 
   return (
-    <Wrapper viewing={isViewing}>
-      <PinHintDot>
+    <div className={s.wrapper}>
+      <div className={s.pinHintDot}>
         <ArticlePinLabel isPinned={isPinned} top={0} left={0} />
-      </PinHintDot>
+      </div>
 
-      <ViewedHintDot>
+      <div className={s.viewHintDot}>
         <ArticleReadLabel viewed={viewerHasViewed} top={0} right={0} />
-      </ViewedHintDot>
+      </div>
 
-      <Br top={5} />
-      <Title
+      <div className="mt-1" />
+      <Link
+        className={s.titleLink}
         onClick={(e) => {
           e.preventDefault()
           previewArticle(data)
@@ -44,22 +46,17 @@ const ArticleCard: FC<TProps> = ({ data }) => {
         href={`/${slug}/${THREAD.POST}/${innerId}`}
       >
         {title}
-      </Title>
+      </Link>
 
-      <div
-        onClick={() => {
-          previewArticle(data)
-        }}
-      >
-        {cutRest(digest, 150)}
-      </div>
+      <div onClick={() => previewArticle(data)}>{cutRest(digest, 150)}</div>
 
+      <div className="mt-1" />
       <Br top={4} />
       <ArticleImgWindow />
-      <Br top={18} />
+      <div className="mt-4" />
       <div className="grow" />
       <Footer data={data} />
-    </Wrapper>
+    </div>
   )
 }
 
