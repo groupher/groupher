a5c6a3bdd57c54505ea4d66423bb6c1f393a4908
mydearxym <mydearxym@qq.com>
2025-01-14 23:05:27 +0800

    chore: wip

--- src/containers/thread/DashboardThread/salon/cms/docs/faq/index.ts
+++ src/containers/thread/DashboardThread/salon/cms/docs/faq/index.ts
@@ -1,6 +1,17 @@
 import styled, { css, theme } from '~/css'
 import { MarkdownStyles } from '~/widgets/Common'
 
+import useTwBelt from '~/hooks/useTwBelt'
+
+export default () => {
+  const { cn, fg } = useTwBelt()
+
+  return {
+    wrapper: cn('row-center w-11/12 p-2.5', fg('text.digest')),
+    inner: 'w-96 mt-5',
+  }
+}
+
 export const Wrapper = styled(MarkdownStyles)`
   ${css.row('justify-center')};
   width: 85%;
