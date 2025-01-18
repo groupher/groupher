d07b74c695e54f0d37809c37859e0f92ac1ab4c5
mydearxym <mydearxym@qq.com>
2025-01-18 23:26:17 +0800

    chore: wip

--- src/widgets/GlobalLayout/Wallpaper.tsx
+++ src/widgets/GlobalLayout/Wallpaper.tsx
@@ -12,6 +12,5 @@ export default () => {
 
   return <div className={s.wrapper} style={{ background, ...fmt2CompStyle(effect) }} />
   // for use style object not passing props
-  // @link see https://github.com/styled-components/styled-components/issues/3315#issuecomment-885977691
   // return <Wrapper style={{ background }} $effect={effect} $theme={theme} />
 }
