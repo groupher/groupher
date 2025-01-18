6cc1d2cbdae38cfb1fc4888b70b0bfb162b82cd0
mydearxym <mydearxym@qq.com>
2025-01-18 23:33:28 +0800

    chore: wip

--- utils/constant/i18n.tsx
+++ utils/constant/i18n.tsx
@@ -1,5 +1,3 @@
-import styled, { css, theme } from '~/css'
-
 import type { TLocale, TSelectOption } from '~/spec'
 
 import PandaSVG from '~/icons/Panda'
@@ -16,60 +14,30 @@ export const LOCALE = {
   ES: 'es',
 } as Record<Uppercase<TLocale>, TLocale>
 
-const commonIcon = (comp) => {
-  return styled(comp)`
-    ${css.size(18)};
-    margin-right: 6px;
-    cursor: pointer;
-  `
-}
-
-const Icon = {
-  Guard: styled(commonIcon(GuardSVG))`
-    ${css.size(19)};
-  `,
-  Russia: styled(commonIcon(RussiaSVG))`
-    ${css.size(19)};
-  `,
-  Hua: styled(commonIcon(HuaSVG))`
-    fill: ${theme('article.digest')};
-    margin-top: 1px;
-  `,
-  Panda: styled(commonIcon(PandaSVG))`
-    margin-top: 4px;
-  `,
-  Spain: styled(commonIcon(SpainSVG))`
-    ${css.size(16)};
-    margin-top: 1px;
-    margin-left: 1px;
-    margin-right: 2px;
-  `,
-}
-
 export const LANGS_OPTIONS = [
   {
     label: 'English',
     value: LOCALE.EN,
-    icon: Icon.Guard,
+    icon: GuardSVG,
   },
   {
     label: '简体中文',
     value: LOCALE.ZH,
-    icon: Icon.Panda,
+    icon: PandaSVG,
   },
   {
     label: '繁体中文',
     value: LOCALE['ZH-HANT'],
-    icon: Icon.Hua,
+    icon: HuaSVG,
   },
   {
     label: 'Русский',
     value: LOCALE.RU,
-    icon: Icon.Russia,
+    icon: RussiaSVG,
   },
   {
     label: 'Español',
     value: LOCALE.ES,
-    icon: Icon.Spain,
+    icon: SpainSVG,
   },
 ] as TSelectOption[]
