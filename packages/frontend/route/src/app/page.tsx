58dd762c17b4e7413c30b74a73ea8a722327896d
mydearxym <mydearxym@qq.com>
2025-01-28 15:14:13 +0800

    chore(app): route init

---
+++ packages/frontend/route/src/app/page.tsx
@@ -0,0 +1,10 @@
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = 'force-static'
+
+export default () => {
+  return (
+    <>
+      <h2>Route page</h2>
+    </>
+  )
+}
