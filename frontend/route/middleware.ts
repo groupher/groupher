eb621b13a7168c03492bff821082c2b99045dcc7
mydearxym <mydearxym@qq.com>
2025-02-03 11:36:11 +0800

    chore: update static url method

--- frontend/route/middleware.ts
+++ frontend/route/middleware.ts
@@ -4,11 +4,13 @@ import type { NextRequest } from 'next/server'
 const LANDING_SITE = process.env.LANDING_SITE || 'https://landing.groupher.com'
 const MAIN_SITE = process.env.MAIN_SITE || 'https://main.groupher.com'
 
+const LANDING_STATIC_SIGN = new URL(LANDING_SITE).hostname.split('.')[0]
+
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
 
   const isLandingRequest =
-    request.nextUrl.pathname === '/' || request.nextUrl.pathname.startsWith('/landing/_next/static')
+    request.nextUrl.pathname === '/' || request.nextUrl.pathname.startsWith(LANDING_STATIC_SIGN)
 
   if (isLandingRequest) {
     return NextResponse.rewrite(new URL(pathname, LANDING_SITE))
