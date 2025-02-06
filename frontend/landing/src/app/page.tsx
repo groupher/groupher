eb7c772aba327270b1df5aaf92084fb6acc665f4
mydearxym <mydearxym@qq.com>
2025-02-06 17:46:39 +0800

    refactor(fe): make landing export to static

--- frontend/landing/src/app/page.tsx
+++ frontend/landing/src/app/page.tsx
@@ -1,6 +1,3 @@
-// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
-export const dynamic = 'force-static'
-
 import Landing from './Landing'
 
 export default () => {
