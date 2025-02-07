133ac9ef868de6c64c1946fccfda5f2b2e3eb7a7
mydearxym <mydearxym@qq.com>
2025-02-07 11:52:55 +0800

    chore: wip

--- frontend/route/middleware.ts
+++ frontend/route/middleware.ts
@@ -1,26 +1,25 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-import { includes } from 'ramda'
+import { includes, startsWith } from 'ramda'
 
 const LANDING_SITE = process.env.LANDING_SITE || 'https://landing.groupher.com'
 const MAIN_SITE = process.env.MAIN_SITE || 'https://main.groupher.com'
 const STATIC_PATHS = ['/', '/pricing']
 
-const getLandingStaticSign = (url) => {
+const getNextStaticSign = (url) => {
   const subdomain = new URL(url).hostname.split('.')[0]
 
   return `/${subdomain}/_next/static`
 }
 
-const LANDING_STATIC_SIGN = getLandingStaticSign(LANDING_SITE)
+const LANDING_STATIC_SIGN = getNextStaticSign(LANDING_SITE)
 
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
 
   const isStaticRequest =
-    includes(request.nextUrl.pathname, STATIC_PATHS) ||
-    request.nextUrl.pathname.startsWith(LANDING_STATIC_SIGN)
+    includes(pathname, STATIC_PATHS) || startsWith(LANDING_STATIC_SIGN, pathname)
 
   if (isStaticRequest) {
     return NextResponse.rewrite(new URL(pathname, LANDING_SITE))
