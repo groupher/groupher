759408ded062fe928ed93ae392873450352a275a
xieyimian <mydearxym@qq.com>
2025-02-08 21:50:25 +0800

    refactor(dev): adjust makefile & clean up (#303)
    * chore: wip
    
    * chore: wip
    
    * chore: wip
    
    * chore: wip
    
    * chore: wip
    
    * chore: clean up
    
    * chore: clean up

--- frontend/route/middleware.ts
+++ frontend/route/middleware.ts
@@ -1,24 +1,36 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+import { includes, startsWith } from 'ramda'
+
 const LANDING_SITE = process.env.LANDING_SITE || 'https://landing.groupher.com'
 const MAIN_SITE = process.env.MAIN_SITE || 'https://main.groupher.com'
+const STATIC_PATHS = ['/', '/pricing', '/book-demo']
 
-const getLandingStaticSign = (url) => {
+const getNextStaticSign = (url) => {
   const subdomain = new URL(url).hostname.split('.')[0]
 
   return `/${subdomain}/_next/static`
 }
 
-const LANDING_STATIC_SIGN = getLandingStaticSign(LANDING_SITE)
+const LANDING_STATIC_SIGN = getNextStaticSign(LANDING_SITE)
+
+/**
+ * @description
+ * Check if the page is a static page from groupher landing.
+ * @param {NextRequest} request - The request object.
+ * @returns {boolean} - If the page is a static page from groupher landing.
+ */
+const checkStaticPage = (pathname: string): boolean => {
+  return includes(pathname, STATIC_PATHS) || startsWith(LANDING_STATIC_SIGN, pathname)
+}
 
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
 
-  const isLandingRequest =
-    request.nextUrl.pathname === '/' || request.nextUrl.pathname.startsWith(LANDING_STATIC_SIGN)
+  const isStaticPage = checkStaticPage(pathname)
 
-  if (isLandingRequest) {
+  if (isStaticPage) {
     return NextResponse.rewrite(new URL(pathname, LANDING_SITE))
   }
 
