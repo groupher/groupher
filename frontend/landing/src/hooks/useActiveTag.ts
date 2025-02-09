a89a4ef69afb68ce0447f8f504d7ce6bfcd3b047
mydearxym <mydearxym@qq.com>
2025-02-09 15:02:24 +0800

    chore: clean up

--- frontend/landing/src/hooks/useActiveTag.ts
+++
@@ -1,9 +0,0 @@
-import type { TTag } from '~/spec'
-
-import useSubStore from '~/hooks/useSubStore'
-
-export default (): TTag => {
-  const store = useSubStore('viewing')
-
-  return store.activeTag
-}
--- frontend/landing/src/spec/i18n.d.ts
+++
@@ -1,7 +0,0 @@
-import type langJson from '~/i18n/en'
-
-export type TLocale = 'en' | 'zh' | 'zh-hant' | 'ru' | 'es'
-
-// type TlangJson = typeof langJson
-
-export type TTransKey = keyof typeof langJson
