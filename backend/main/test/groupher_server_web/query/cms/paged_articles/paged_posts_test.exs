369a08dcd4116fbfe66c7bf4760b1d4b3525577e
mydearxym <mydearxym@qq.com>
2025-03-07 15:29:23 +0800

    chore: update ci

--- backend/main/test/groupher_server_web/query/cms/paged_articles/paged_posts_test.exs
+++ backend/main/test/groupher_server_web/query/cms/paged_articles/paged_posts_test.exs
@@ -425,12 +425,12 @@ defmodule GroupherServer.Test.Query.PagedArticles.PagedPosts do
       assert results |> Map.get("totalCount") == expect_count
     end
 
+    @tag :wip
     test "THIS_WEEK option should work", ~m(guest_conn)a do
       variables = %{filter: %{when: "THIS_WEEK"}}
       results = guest_conn |> query_result(@query, variables, "pagedPosts")
 
-      # TODO, fix later
-      # assert results |> Map.get("totalCount") == @today_count
+      assert results |> Map.get("totalCount") == @today_count
     end
 
     test "THIS_MONTH option should work", ~m(guest_conn post_last_month)a do
