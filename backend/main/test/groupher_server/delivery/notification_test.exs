e0c370685e5860857bd9b9ea4393d04a6561e446
mydearxym <mydearxym@qq.com>
2025-03-07 16:03:27 +0800

    chore: clean up

--- backend/main/test/groupher_server/delivery/notification_test.exs
+++ backend/main/test/groupher_server/delivery/notification_test.exs
@@ -104,7 +104,6 @@ defmodule GroupherServer.Test.Delivery.Notification do
       assert user3 |> user_exist_in?(notify.from_users)
     end
 
-    @tag :wip
     test "different notify should not be merged", ~m(user user2 user3 notify_attrs)a do
       {:ok, _} = Delivery.send(:notify, notify_attrs, user2)
       notify_attrs = notify_attrs |> Map.put(:action, :collect)
@@ -121,7 +120,6 @@ defmodule GroupherServer.Test.Delivery.Notification do
       assert user2 |> user_exist_in?(notify2.from_users)
     end
 
-    @tag :wip
     test "notify not in @notify_group_interval_hour should not be merged",
          ~m(user user2 user3 notify_attrs)a do
       {:ok, notify} = Delivery.send(:notify, notify_attrs, user2)
