63499118a1349dbb6cdce8e963d4bf40f9dfd4f0
mydearxym <mydearxym@qq.com>
2025-03-21 11:18:04 +0800

    chore: typo

--- backend/main/test/groupher_server/cms/comments/changelog_comment_test.exs
+++ backend/main/test/groupher_server/cms/comments/changelog_comment_test.exs
@@ -684,7 +684,7 @@ defmodule GroupherServer.Test.CMS.Comments.ChangelogComment do
       assert paged_comments.total_count == total_count
     end
 
-    test "paged article comments should not contains folded and repoted comments",
+    test "paged article comments should not contains folded and reported comments",
          ~m(community user changelog)a do
       total_count = 15
       page_number = 1
