3dc66f062d746058c28e843d4fe385cd9a549769
mydearxym <mydearxym@qq.com>
2025-05-17 16:17:37 +0800

    chore: clean up middlewares

--- frontend/dashboard/src/middleware.ts
+++ frontend/dashboard/src/middleware.ts
@@ -1,11 +1,11 @@
 import type { NextRequest } from 'next/server'
 
-import { applyMiddleware } from './middlewares/helper'
+import { applyMiddleware } from '~/middlewares/helper'
 // import { themeMiddleware } from './middlewares/theme'
-import { queryWhitelistMiddleware } from './middlewares/query-whitelist'
-import { oopsMiddleware } from './middlewares/oops'
-import { avoidScanMiddleware } from './middlewares/avoid-scan'
-import { urlPeekMiddleware } from './middlewares/url-peek'
+import { queryWhitelistMiddleware } from '~/middlewares/query-whitelist'
+import { oopsMiddleware } from '~/middlewares/oops'
+import { avoidScanMiddleware } from '~/middlewares/avoid-scan'
+import { urlPeekMiddleware } from '~/middlewares/url-peek'
 
 export function middleware(request: NextRequest) {
   // middleware in this array will be applied in order
