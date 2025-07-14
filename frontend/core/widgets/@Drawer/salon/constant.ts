46353d5d00d923fac1edeebc0ab710672ea84725
mydearxym <mydearxym@qq.com>
2025-07-14 15:42:51 +0800

    chore: improve ts types

--- frontend/core/widgets/@Drawer/salon/constant.ts
+++ frontend/core/widgets/@Drawer/salon/constant.ts
@@ -13,7 +13,6 @@ export const ARTICLE_VIEWER_TYPES = reduce(
 
 export const ARTICLE_THREAD_CURD_TYPES = reduce(
   concat,
-  // @ts-ignore
   [...ARTICLE_VIEWER_TYPES],
   keys(ARTICLE_THREAD).map((T) => [TYPE.DRAWER[`${T}_CREATE`], TYPE.DRAWER[`${T}_EDIT`]]),
 )
