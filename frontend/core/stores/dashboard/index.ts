c9e6973ba0f5bab129e39c826a03aff3e16d7995
mydearxym <mydearxym@qq.com>
2025-10-25 09:57:13 +0800

    chore: improve dashboard store init

--- frontend/core/stores/dashboard/index.ts
+++ frontend/core/stores/dashboard/index.ts
@@ -145,61 +145,52 @@ export const settingsFields: TSettingsFields = {
 }
 
 export default (init: TInit = {}): TStore => {
-  const initialStore: TStore = {
-    ...settingsFields,
-    curTab: DASHBOARD_ROUTE.INFO,
-
-    initFilled: false,
-    original: settingsFields,
-
-    savingField: null,
-    saving: false,
-    loading: false,
-    baseInfoTab: DASHBOARD_BASEINFO_ROUTE.BASIC,
-
-    aliasTab: DASHBOARD_ALIAS_ROUTE.THREAD,
-    seoTab: DASHBOARD_SEO_ROUTE.SEARCH_ENGINE,
-    docTab: DASHBOARD_DOC_ROUTE.TABLE,
-    layoutTab: DASHBOARD_LAYOUT_ROUTE.GENERAL,
-    broadcastTab: DASHBOARD_BROADCAST_ROUTE.GLOBAL,
-
-    overview: DEFAULT_OVERVIEW,
-
-    editingTag: null,
-    settingTag: null,
-    editingAlias: null,
-    editingLink: null,
-    editingLinkMode: CHANGE_MODE.CREATE,
-
-    editingGroup: null,
-    editingGroupIndex: null,
-    editingFAQIndex: null,
-    editingFAQ: null,
-
-    queringMediaReportIndex: null,
-
-    batchSelectedIDs: [],
-    pagedCommunities: EMPTY_PAGED_COMMUNITIES,
-    pagedPosts: EMPTY_PAGED_ARTICLES,
-    pagedDocs: EMPTY_PAGED_ARTICLES,
-    pagedChangelogs: EMPTY_PAGED_ARTICLES,
-
-    demoAlertEnable: false,
-    activeModerator: null,
-    allModeratorRules: '{}',
-    allRootRules: '{}',
-
-    ...init,
-
-    commit: (patch: Partial<TStore>): void => {
-      Object.assign(store, patch)
+  const states = Object.assign(
+    {
+      ...settingsFields,
+      curTab: DASHBOARD_ROUTE.INFO,
+      initFilled: false,
+      original: settingsFields,
+      savingField: null,
+      saving: false,
+      loading: false,
+      baseInfoTab: DASHBOARD_BASEINFO_ROUTE.BASIC,
+      aliasTab: DASHBOARD_ALIAS_ROUTE.THREAD,
+      seoTab: DASHBOARD_SEO_ROUTE.SEARCH_ENGINE,
+      docTab: DASHBOARD_DOC_ROUTE.TABLE,
+      layoutTab: DASHBOARD_LAYOUT_ROUTE.GENERAL,
+      broadcastTab: DASHBOARD_BROADCAST_ROUTE.GLOBAL,
+      overview: DEFAULT_OVERVIEW,
+      editingTag: null,
+      settingTag: null,
+      editingAlias: null,
+      editingLink: null,
+      editingLinkMode: CHANGE_MODE.CREATE,
+      editingGroup: null,
+      editingGroupIndex: null,
+      editingFAQIndex: null,
+      editingFAQ: null,
+      queringMediaReportIndex: null,
+      batchSelectedIDs: [],
+      pagedCommunities: EMPTY_PAGED_COMMUNITIES,
+      pagedPosts: EMPTY_PAGED_ARTICLES,
+      pagedDocs: EMPTY_PAGED_ARTICLES,
+      pagedChangelogs: EMPTY_PAGED_ARTICLES,
+      demoAlertEnable: false,
+      activeModerator: null,
+      allModeratorRules: '{}',
+      allRootRules: '{}',
+      commit(patch: Partial<TStore>): void {
+        Object.assign(store, patch)
+      },
+      debug() {
+        store.editingLink = null
+        store.headerLinks = []
+      },
     },
-    debug: () => {
-      store.editingLink = null
-      store.headerLinks = []
-    },
-  }
+    init,
+  )
 
-  const store = proxy(initialStore)
+  const store = proxy(states)
   return store
 }
