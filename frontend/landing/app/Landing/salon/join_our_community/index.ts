6712b12a43a67c91d1f70d96e8f1c25354a93dba
mydearxym <mydearxym@qq.com>
2025-10-31 12:50:38 +0800

    chore: dark theme adjust

--- frontend/landing/app/Landing/salon/join_our_community/index.ts
+++ frontend/landing/app/Landing/salon/join_our_community/index.ts
@@ -5,20 +5,14 @@ export { cn } from '~/css'
 
 export default () => {
   const { isDarkTheme } = useTheme()
-  const { cn, bg, fg, landingTitle, br, sexyBorder } = useTwBelt()
+  const { cn, fg, landingTitle, sexyBorder } = useTwBelt()
 
   return {
     wrapper: cn('column-align-both w-full relative mt-36'),
     slogan: 'column w-2/5',
-    topping: cn(
-      'text-xs border mb-3 px-3.5 py-1.5 rounded-lg',
-      fg('text.title'),
-      br('divider'),
-      bg('card'),
-    ),
     title: landingTitle(),
     desc: cn('text-base mt-3', fg('text.digest'), isDarkTheme && 'opacity-65'),
     divider: sexyBorder(35, 'w-10/12'),
-    paper: cn('row-center relative w-9/12 h-auto py-4.5', bg('card')),
+    paper: cn('row-center relative w-9/12 h-auto py-4.5'),
   }
 }
