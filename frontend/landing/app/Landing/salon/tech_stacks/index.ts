3212f81ed7ece5ed4ea9577ec1a57ebe97d9b176
mydearxym <mydearxym@qq.com>
2025-11-06 22:37:13 +0800

    chore(landing): OO section style adjust

--- frontend/landing/app/Landing/salon/tech_stacks/index.ts
+++ frontend/landing/app/Landing/salon/tech_stacks/index.ts
@@ -5,20 +5,21 @@ export { cn } from '~/css'
 
 export default () => {
   const { isDarkTheme } = useTheme()
-  const { cn, fg, bg, landingTitle } = useTwBelt()
+  const { cn, fg, bg, landingTitle, shadow } = useTwBelt()
 
   return {
     wrapper: cn('column-align-both w-full mt-32'),
     wall: cn(
-      'align-both w-full mt-20 mb-12 relative border-12',
-      isDarkTheme ? 'border-zinc-800' : 'border-zinc-200',
+      'align-both w-full mt-16 mb-12 relative border-12 overflow-hidden rounded-4xl',
+      isDarkTheme ? 'border-zinc-800 border-x-20' : 'border-zinc-200',
       bg('menuInvertBg'),
     ),
     inner: cn(
       'align-both justify-between relative w-full h-auto border',
-      'pb-20 pt-12 px-4 overflow-hidden rounded-3xl',
+      'pb-20 pt-12 px-4',
+      shadow('card'),
     ),
-    mask: 'z-30 absolute w-3/4 h-full left-0 opacity-20 pointer-events-none blur-xl',
+    mask: 'z-30 absolute w-3/4 h-full left-0 opacity-20 pointer-events-none blur-xl rounded-3xl',
     innerBgWrapper: cn(
       'absolute top-0 left-0 w-full h-full overflow-hidden rounded-xl pointer-events-none',
       isDarkTheme && 'invert',
@@ -26,7 +27,5 @@ export default () => {
     slogan: 'column-align-both',
     title: landingTitle(),
     desc: cn('text-lg mt-3', fg('text.digest'), isDarkTheme && 'opacity-65'),
-    cadBg: cn('absolute top-0 h-full object-cover z-0 mix-blend-soft-light'),
-    cadDark: isDarkTheme && 'opacity-40',
   }
 }
