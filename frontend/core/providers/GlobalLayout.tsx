c288269aadf1e450480f81c0aaba295e054a62c5
xieyimian <mydearxym@qq.com>
2025-12-24 15:59:28 +0800

    fix(fe): topbar style issue (#371)
    * fix: topbar style issue
    
    * chore: fmt naming
    
    * chore: type

--- frontend/core/providers/GlobalLayout.tsx
+++ frontend/core/providers/GlobalLayout.tsx
@@ -1,15 +1,13 @@
 'use client'
 
 import type { FC, ReactNode } from 'react'
-import GlobalLayoutWrapper from '~/widgets/GlobalLayout'
+import RealGlobalLayout from '~/widgets/GlobalLayout'
 
 type TProps = {
   mainBlock?: FC<{ children: ReactNode }>
   children?: ReactNode
 }
 
-const GlobalLayout: FC<TProps> = ({ children, mainBlock = undefined }) => {
-  return <GlobalLayoutWrapper mainBlock={mainBlock}>{children}</GlobalLayoutWrapper>
+export default ({ children, mainBlock = undefined }: TProps) => {
+  return <RealGlobalLayout mainBlock={mainBlock}>{children}</RealGlobalLayout>
 }
-
-export default GlobalLayout
--- frontend/core/widgets/CommunityDigest/salon/dashboard_layout/index.ts
+++ frontend/core/widgets/CommunityDigest/salon/dashboard_layout/index.ts
@@ -1,10 +1,10 @@
 import useTwBelt from '~/hooks/useTwBelt'
 
 export default () => {
-  const { cn, sexyBorder } = useTwBelt()
+  const { cn } = useTwBelt()
 
   return {
-    wrapper: cn('w-full h-16 border-b', sexyBorder(25)),
+    wrapper: cn('w-full h-16'),
     inner: cn('column justify-center mb-2 w-full'),
     content: 'column-center justify-between',
     baseInfo: 'row-center-between w-full pt-2.5',
