851d00660acc594ce4f3a96b8e97c9558769ceb6
mydearxym <mydearxym@qq.com>
2025-12-24 14:32:24 +0800

    chore: debug 4 [deploy:main]

--- frontend/main/src/app/layout.tsx
+++ frontend/main/src/app/layout.tsx
@@ -5,7 +5,7 @@ import RootLayoutShell from '~/widgets/RootLayoutShell'
 import '~/tailwind/global.css'
 
 export const metadata: Metadata = {
-  title: 'Groupher | 让你的产品听见用户的声音.',
+  title: 'Groupher | 让你的产品听见用户的声音',
   description: '讨论区、看板、更新日志、帮助文档多合一，收集沉淀用户反馈，助你打造更好的产品。',
 }
 
