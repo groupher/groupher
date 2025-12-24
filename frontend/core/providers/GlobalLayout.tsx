bbb989d57e62ea772442dcb8dcd3e3f60f0a3142
mydearxym <mydearxym@qq.com>
2025-12-24 15:48:56 +0800

    fix: topbar style issue

--- frontend/core/providers/GlobalLayout.tsx
+++ frontend/core/providers/GlobalLayout.tsx
@@ -1,15 +1,13 @@
 'use client'
 
 import type { FC, ReactNode } from 'react'
-import GlobalLayoutWrapper from '~/widgets/GlobalLayout'
+import GlobalLayout from '~/widgets/GlobalLayout'
 
 type TProps = {
   mainBlock?: FC<{ children: ReactNode }>
   children?: ReactNode
 }
 
-const GlobalLayout: FC<TProps> = ({ children, mainBlock = undefined }) => {
-  return <GlobalLayoutWrapper mainBlock={mainBlock}>{children}</GlobalLayoutWrapper>
+export default ({ children, mainBlock = undefined }: TProps) => {
+  return <GlobalLayout mainBlock={mainBlock}>{children}</GlobalLayout>
 }
-
-export default GlobalLayout
--- frontend/dashboard/src/app/[community]/dashboard/salon/index.ts
+++ frontend/dashboard/src/app/[community]/dashboard/salon/index.ts
@@ -5,7 +5,7 @@ export default () => {
 
   return {
     wrapper: cn('column-center justify-start min-h-full w-full'),
-    inner: cn('column-center w-full mt-14'),
+    inner: cn('column-center w-full'),
     content: 'row w-full min-h-screen',
     main: 'ml-28 mt-7 grow bg-transparent',
   }
