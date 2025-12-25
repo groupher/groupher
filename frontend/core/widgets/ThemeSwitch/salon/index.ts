6910254595fac527d1f6ddb94bbb03f364817292
mydearxym <mydearxym@qq.com>
2025-12-25 12:10:46 +0800

    chore: clean up

--- frontend/core/widgets/ThemeSwitch/salon/index.ts
+++ frontend/core/widgets/ThemeSwitch/salon/index.ts
@@ -6,7 +6,7 @@ type TProps = TSpace
 export { cn } from '~/css'
 
 export default ({ ...spacing }: TProps) => {
-  const { cn, bg, fill, margin, menu } = useTwBelt()
+  const { cn, bg, fill, margin } = useTwBelt()
 
   return {
     wrapper: cn('', margin(spacing)),
@@ -16,19 +16,15 @@ export default ({ ...spacing }: TProps) => {
       'touch-manipulation outline-offset-4 pointer',
       `hover:${bg('hoverBg')}`,
     ),
+    iconBox: cn(
+      'size-6 align-both pointer rounded border border-transparent',
+      `hover:${bg('hoverBg')}`,
+    ),
+
     icon: cn(
       'size-5 active:scale-90 trans-all-200',
       fill('text.digest'),
       `group-hover:${fill('text.title')}`,
     ),
-    iconBox: cn(
-      'size-6 align-both pointer rounded border border-transparent',
-      `hover:${bg('hoverBg')}`,
-    ),
-    selectWrapper: 'row-center gap-x-1 px-1',
-    selectBox: cn(menu('bar'), 'align-both size-8 m-0 p-0'),
-    selectIcon: cn(menu('icon'), 'size-6 m-0 opacity-80 scale-90'),
-    activeBox: cn(menu('activeBox')),
-    activeIcon: cn(menu('activeIcon')),
   }
 }
