9001736f2dc531a28de4af4410d3a0e27a618905
mydearxym <mydearxym@qq.com>
2025-12-31 10:09:28 +0800

    fix: thread missing in layout parse

--- frontend/core/hooks/useViewingThread.ts
+++ frontend/core/hooks/useViewingThread.ts
@@ -1,17 +1,35 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import METRIC from '~/const/metric'
 import { THREAD } from '~/const/thread'
-import useArticleList from '~/hooks/useArticleList'
 import useMetric from '~/hooks/useMetric'
 
 import type { TThread } from '~/spec'
 
+const isThread = (value: string): value is TThread => {
+  return Object.values(THREAD).includes(value as TThread)
+}
+
+const getThreadFromPathname = (pathname: string): TThread | null => {
+  const segments = pathname.split('/').filter(Boolean)
+  const maybeThread = segments.at(-1)
+
+  if (maybeThread && isThread(maybeThread)) {
+    return maybeThread
+  }
+
+  return null
+}
+
 export default (): TThread => {
+  const pathname = usePathname()
+
   const metric = useMetric()
   if (metric === METRIC.LANDING) {
     return THREAD.POST
   }
+  const thread = getThreadFromPathname(pathname)
 
-  const articleList$ = useArticleList()
-
-  return articleList$.thread
+  return thread ?? THREAD.POST
 }
