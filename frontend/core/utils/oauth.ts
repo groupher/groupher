50c3e1b7b967ee540c1fa3063b3543f567b8f3c9
mydearxym <mydearxym@qq.com>
2026-01-06 13:55:37 +0800

    refactor(fe): clean up auth-cleanup endpoint

--- frontend/core/utils/oauth.ts
+++ frontend/core/utils/oauth.ts
@@ -1,15 +1,6 @@
 import { signIn as authSignIn, signOut as authSignOut } from 'next-auth/react'
 import type { TOauthProvider } from '~/spec'
 
-// const _deleteCookie = async () => {
-//   await fetch('/api/auth-cleanup', {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   })
-// }
-
 export const signOut = async () => {
   await authSignOut({ redirect: false })
 
