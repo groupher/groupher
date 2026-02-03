3a437b6cbf13aff690d59d7757e1efd2717ef8d0
mydearxym <mydearxym@qq.com>
2026-02-03 12:15:53 +0800

    fix: broken test

--- frontend/core/hooks/useDidMount/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { renderHook, waitFor } from '@testing-library/react'
-import useDidMount from '~/hooks/useDidMount'
-
-it('returns false on first render and true after mount', async () => {
-  const { result } = renderHook(() => useDidMount())
-
-  await waitFor(() => expect(result.current).toBe(true))
-})
