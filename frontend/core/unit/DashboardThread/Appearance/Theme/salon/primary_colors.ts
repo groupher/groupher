b9cef67cf4d60e35da6c5ee6f67ec89326ed72c2
mydearxym <mydearxym@qq.com>
2026-05-21 23:41:36 +0800

    style(fe): align theme color controls

--- frontend/core/unit/DashboardThread/Appearance/Theme/salon/primary_colors.ts
+++ frontend/core/unit/DashboardThread/Appearance/Theme/salon/primary_colors.ts
@@ -6,13 +6,13 @@ export default function useSalon() {
   const { cn, primary, fg } = useTwBelt()
 
   return {
-    wrapper: 'row align-start justify-between',
+    wrapper: 'row align-start gap-4',
     head: 'row-center',
     subHead: 'mt-0.5 mb-4',
-    left: 'w-3/5 pr-6',
+    left: 'w-2/5 min-w-2/5',
     right: 'w-2/5',
     title: cn('text-sm ml-3', fg('title')),
-    desc: cn('text-sm pl-0.5 mt-3', fg('digest')),
+    desc: cn('text-sm mt-3', fg('digest')),
     ballWrapper: cn('align-both size-9 align-both circle border pointer', primary('borderSoft')),
     subBall: 'size-7',
     colorBall: cn('size-7 circle', primary('bg')),
