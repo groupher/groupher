4c67a815f15d35504cb885cb8940eaee9a895b91
mydearxym <mydearxym@qq.com>
2026-05-22 14:04:10 +0800

    style(fe): fix theme selector hover states

--- frontend/core/widgets/ThemeSectionSelector/salon/index.ts
+++ frontend/core/widgets/ThemeSectionSelector/salon/index.ts
@@ -4,15 +4,15 @@ import type { TSpace } from '~/spec'
 export { cn } from '~/css'
 
 export default function useSalon(spacing: TSpace) {
-  const { cn, fg, bg, fill, margin, sexyVBorder, hover } = useTwBelt()
+  const { cn, fg, bg, margin, sexyVBorder, hover } = useTwBelt()
 
   return {
     wrapper: cn('row-center shrink-0', margin(spacing)),
     section: cn('row-center px-1 py-0.5 transition-colors', hover('box')),
     sectionActive: cn('!opacity-100', bg('hoverBg')),
-    title: cn('ml-0.5 whitespace-nowrap text-xs', fg('digest')),
+    title: cn('ml-0.5 whitespace-nowrap text-xs', hover('fg')),
     active: fg('title'),
-    icon: cn('size-3.5', fill('digest')),
+    icon: cn('size-3.5', hover('icon')),
     divider: cn(sexyVBorder(35), 'h-5 ml-0.5 mr-0.5'),
   }
 }
