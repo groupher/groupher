b3c45bcf4a5a0ddd2fa6056a73294b95eaecb6f3
mydearxym <mydearxym@qq.com>
2026-06-02 12:29:05 +0800

    feat(fe): add resizable docs side tree

--- frontend/core/unit/DashboardThread/CMS/Docs/SideTreeEditor/index.tsx
+++ frontend/core/unit/DashboardThread/CMS/Docs/SideTreeEditor/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import type { FC } from 'react'
+import { Group as PanelGroup, Panel, Separator } from 'react-resizable-panels'
 
 import Group from './Group'
 import GroupAdder from './GroupAdder'
@@ -27,29 +28,50 @@ const SideTreeEditor: FC = () => {
   } = useSideTreeEditor()
 
   return (
-    <aside className={s.wrapper}>
-      <div className={s.groupList}>
-        {groups.map((group) => (
-          <Group
-            key={group.id}
-            group={group}
-            activeId={activeId}
-            editingTarget={editingTarget}
-            onActivate={activate}
-            onToggle={toggleGroup}
-            onAddChild={addChild}
-            onRenameGroup={renameGroup}
-            onRenameChild={renameChild}
-            onCancelEdit={cancelEdit}
-            onEdit={edit}
-            onChildAction={handleChildAction}
-            onChildStyleChange={updateChildStyle}
-          />
-        ))}
-      </div>
+    <PanelGroup
+      className={s.panelGroup}
+      orientation='horizontal'
+      resizeTargetMinimumSize={{ fine: 12, coarse: 28 }}
+    >
+      <Panel
+        id='docs-side-tree'
+        className={s.sidePanel}
+        defaultSize={180}
+        minSize={120}
+        maxSize={210}
+        groupResizeBehavior='preserve-pixel-size'
+      >
+        <aside className={s.wrapper}>
+          <div className={s.groupList}>
+            {groups.map((group) => (
+              <Group
+                key={group.id}
+                group={group}
+                activeId={activeId}
+                editingTarget={editingTarget}
+                onActivate={activate}
+                onToggle={toggleGroup}
+                onAddChild={addChild}
+                onRenameGroup={renameGroup}
+                onRenameChild={renameChild}
+                onCancelEdit={cancelEdit}
+                onEdit={edit}
+                onChildAction={handleChildAction}
+                onChildStyleChange={updateChildStyle}
+              />
+            ))}
+          </div>
 
-      <GroupAdder onAddGroup={addGroup} />
-    </aside>
+          <GroupAdder onAddGroup={addGroup} />
+        </aside>
+      </Panel>
+
+      <Separator id='docs-side-tree-resizer' className={s.resizeHandle}>
+        <div className={s.resizeLine} />
+      </Separator>
+
+      <Panel id='docs-editor-space' className={s.fillPanel} minSize={0} />
+    </PanelGroup>
   )
 }
 
