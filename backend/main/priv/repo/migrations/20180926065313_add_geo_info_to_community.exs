a72b8a6b2c9565a3d3f4714fe70dc7c713c8b49b
mydearxym <mydearxym@qq.com>
2026-08-04 15:45:20 +0800

    chore(fe/be): fix ci after dependency upgrades

--- backend/main/priv/repo/migrations/20180926065313_add_geo_info_to_community.exs
+++ backend/main/priv/repo/migrations/20180926065313_add_geo_info_to_community.exs
@@ -1,10 +1,9 @@
 defmodule GroupherServer.Repo.Migrations.AddGeoInfoToCommunity do
   use Ecto.Migration
-  alias Helper.GeoPool
 
   def change do
     alter table(:communities) do
-      add(:geo_info, :map, default: %{data: GeoPool.all()})
+      add(:geo_info, :map, default: %{data: []})
     end
   end
 end
--- frontend/main/src/app/api/graphql/route.ts
+++ frontend/main/src/app/api/graphql/route.ts
@@ -1,6 +1,4 @@
 import { proxyGraphQLRequest } from '~/graphql/proxy'
 
-export const dynamic = 'force-dynamic'
-
 export const GET = (request: Request) => proxyGraphQLRequest(request)
 export const POST = (request: Request) => proxyGraphQLRequest(request)
